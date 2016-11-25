600dcc5a26fe4cf351e7a15e9ae0766a851f62a0
Kamil Pek <kamil.pek@gmail.com>
2016-11-25 20:20:23 +0100

    drobne poprawki

--- lab4_funkcjeskrotu/zad2/diff.py
+++ lab4_funkcjeskrotu/zad2/diff.py
@@ -1,13 +1,14 @@
 import sys
 import string
 
-def main():
+def diff():
 	bity1 = raw_input('Podaj pierwszy ciag bitow: ')
 	bity2 = raw_input('Podaj drugi ciag bitow: ')
 
 	dlugosc = len(bity1)
 	roznica = ([x == y for (x, y) in zip(bity1, bity2)].count(False))
+	procent = (float(roznica)/float(dlugosc))*100
 
-	print('Liczba rozniacych sie bitow: ', roznica, ' z ', dlugosc)
+	print "Liczba rozniacych sie bitow: %d z %d, procentowo: %2.f." % (roznica, dlugosc, procent)
 
-main()
+diff()
