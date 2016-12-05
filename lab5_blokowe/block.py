46112ba55fdc9b3bb67b0d46f55d1441a44d8f86
Kamil Pek <kamil.pek@gmail.com>
2016-12-05 21:16:40 +0100

    lab5 male postepy

--- lab5_blokowe/block.py
+++ lab5_blokowe/block.py
@@ -1,15 +1,36 @@
+# python3 block.py
+# Kamil Pek 231050
+# 05.12.201
+
 from PIL import Image
 
-img = Image.open("plain.bmp")
+if __name__ == '__main__':
+    img = Image.open("plain.bmp")
+    pixels = img.load() # create the pixel map
 
-pixels = img.load() # create the pixel map
+    for i in range(img.size[0]):    # for every pixel:
+        for j in range(img.size[1]):
+            if (i != 0) & (j != 0):
+                k = i - 1
+                l = j - 1
+                # print (k, l, "!=0")
+            else:
+                k = i
+                l = j
+                # print (k, l, "=0")
+            pixa1 = (pixels[i,j][0])
+            pixa2 = (pixels[i,j][1])
+            pixa3 = (pixels[i,j][2])
+            pixb1 = (pixels[k,l][0])
+            pixb2 = (pixels[k,l][1])
+            pixb3 = (pixels[k,l][2])
+            pix1 = pixa1 ^ pixa1
+            pix2 = pixa2 ^ pixa2
+            pix3 = pixa3 ^ pixa3
+            pixels[i,j] = (pix1, pix2, pix3) # set the colour accordingly
+            # pixels[i,j] = (pixa1, pixa2, pixa3) # set the colour accordingly
+            # pixels[i,j] = (pixb1, pixb2, pixb3) # set the colour accordingly
+            pixels[i,j] = hash(pixels[i,j])
 
-for i in range(img.size[0]):    # for every pixel:
-    for j in range(img.size[1]):
-        pix1 = ((pixels[i,j][0])+77)%255
-        pix2 = ((pixels[i,j][1])+22)%255
-        pix3 = ((pixels[i,j][2])+11)%255
-        pixels[i,j] = (pix1, pix2, pix3) # set the colour accordingly
-
-img.show()
-img.save("example.bmp")
+    img.show()
+    img.save("example.bmp")
